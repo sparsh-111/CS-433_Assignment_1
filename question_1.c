10364b8d0cec2acbef723f22973cfdc1259a449e
sparsh-111 <89207720+sparsh-111@users.noreply.github.com>
2023-09-11 01:55:36 +0530

    Update question_1.c

--- question_1.c
+++ question_1.c
@@ -12,12 +12,11 @@ void process_packet(unsigned char* buffer, int size) {
     struct ip *iph = (struct ip*)buffer;
     if (iph->ip_p == IPPROTO_TCP) {
         struct tcphdr *tcph = (struct tcphdr*)(buffer + iph->ip_hl * 4);
-
         printf("Source IP: %s\n", inet_ntoa(iph->ip_src));
-        printf("Destination IP: %s\n", inet_ntoa(iph->ip_dst));
         printf("Source Port: %d\n", ntohs(tcph->th_sport));
+        printf("Destination IP: %s\n", inet_ntoa(iph->ip_dst));
         printf("Destination Port: %d\n", ntohs(tcph->th_dport));
-        printf("----------------------------\n");
+        printf("\n");
     }
 }
 
@@ -25,25 +24,21 @@ int main() {
     int raw_socket;
     struct sockaddr_in server;
     unsigned char buffer[65536]; // Maximum packet size
-
-    // Create a raw socket
+    // Creating  a raw socket
     raw_socket = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
     if (raw_socket == -1) {
-        perror("Socket creation error");
+        perror("Error Creating a Socket");
         exit(1);
     }
-
     while (1) {
         int data_size = recvfrom(raw_socket, buffer, sizeof(buffer), 0, NULL, NULL);
         if (data_size < 0) {
-            perror("Packet receive error");
+            perror("Error recieving a Packet");
             exit(1);
         }
-
         // Process the received packet
         process_packet(buffer, data_size);
     }
-
     close(raw_socket);
     return 0;
 }
